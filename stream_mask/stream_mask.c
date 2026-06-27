1c78d55f7c147be9ba9a8a2a8efc22cd601d7b43
Yuuuuuri01 <hamzaofficer2@gmail.com>
2026-06-27 11:41:25 -0400

    Refactor string handling in stream_mask.c
    Refactor stream_mask.c by replacing custom string functions with standard library functions, improving readability and maintainability.

--- stream_mask/stream_mask.c
+++ stream_mask/stream_mask.c
@@ -1,19 +1,8 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
 
-
-int is_blank(char c)
-{
-    return (c == ' ' || c == '\t' || c == '\n');
-}
-int ft_strlen(char *str)
-{
-    int i = 0;
-    while(str[i])
-        i++;
-    return i;
-}
 int ft_strcmp(char *s1, char *s2)
 {
     while(*s1 && *s2)
@@ -25,135 +14,132 @@ int ft_strcmp(char *s1, char *s2)
     }
     return (*s1 - *s2);
 }
-char *add_to_buff(char *buff, int len, char c)
+int is_space(char c)
+{
+    return (c == ' ' || c == '\t' || c == '\n');
+}
+char *add_to_buff(char *buff, int len, char s)
 {
     char *tmp = realloc(buff, len + 1);
     if(!tmp)
     {
-        free(buff);
         return NULL;
     }
-    tmp[len] = c;
+    tmp[len] = s;
     return tmp;
 }
-void print_it(char *word, int len, char *av)
+void print_mask(char *word, int len1, int len2, int *cmp)
 {
-    if(!word || len == 0) return;
-    
-    word[len] = '\0';
-    int len2 = 0;
+    if(!word || !len1)
+        return;
+    word[len1] = '\0';
     int i = 0;
-
-    if(ft_strcmp(word, av) == 0)
+    if(*cmp)
     {
-        len2 = ft_strlen(av);
         while(i < len2)
         {
             write(1, "*", 1);
             i++;
         }
+        *cmp = 0;
     }
     else
-        write(1, word, len);
+        write(1, word, len1);
+}
+int handle_spaces(char *s)
+{
+    while(*s)
+    {
+        if(*s != ' ' && *s != '\t')
+            return -1;
+        s++;
+    }
+    return 1;
 }
 int stream_mask(char *av)
 {
+    int cmp = 0;
     char *word = NULL;
-    int len = 0;
+    int len1 = 0;
+    int len2 = strlen(av);
     char c;
-    int len2 = ft_strlen(av);
 
-    if(len2 == 0) return 1;
-
+    if(handle_spaces(av) != -1)
+    {
+        while(read(0, &c, 1) > 0)
+        {
+            if(c == ' ' || c == '\t')
+                write(1, "*", 1);
+            else
+                write(1, &c, 1);
+        }
+        return 0;
+    }
     while(read(0, &c, 1) > 0)
     {
-        if(!is_blank(c))
+        if(!is_space(c))
         {
-            word = add_to_buff(word, len, c);
+            word = add_to_buff(word, len1, c);
             if(!word)
             {
-                perror("Error\n");
+                perror("error realloc fail");
                 return 1;
             }
-            len++;
-            if(len == len2)
+            len1++;
+            if(len1 == len2)
             {
-                word = add_to_buff(word, len, '\0');
+                word = add_to_buff(word, len1, '\0');
                 if(!word)
                 {
-                    perror("realloc faild\n");
+                    perror("error realloc");
                     return 1;
                 }
-                if(ft_strcmp(word, av) == 0)
+                else if(ft_strcmp(word, av) == 0)
                 {
-                    print_it(word, len, av);
+                    cmp = 1;
+                    print_mask(word, len1, len2, &cmp);
                     free(word);
                     word = NULL;
-                    len = 0;
+                    len1 = 0;
                 }
             }
         }
         else
         {
-            if(len > 0)
+            if(len1 > 0)
             {
-                word = add_to_buff(word, len, '\0');
-                write(1, word, len);
+                word = add_to_buff(word, len1, '\0');
+                if(!word)
+                {
+                    perror("error");
+                    return 1;
+                }
+                print_mask(word, len1, len2, &cmp);
                 free(word);
                 word = NULL;
-                len = 0;
+                len1 = 0;
             }
             write(1, &c, 1);
         }
     }
-    if(len > 0)
+    if(len1 > 0)
     {
-        word = add_to_buff(word, len, '\0');
-        print_it(word, len, av);
-        free(word);
-    }
-    return 0;
-}
-int space_handle(char *av)
-{
-    char c;
-    char *word = NULL;
-    int len = 0;
-    
-    while(*av)
-    {
-        if(!is_blank(*av))
-            return -1;
-        av++;
-    }
-    while(read(0, &c, 1) > 0)
-    {
-        word = add_to_buff(word, len, c);
+        word = add_to_buff(word, len1, '\0');
         if(!word)
         {
-            perror("ERROR\n");
+            perror("error");
             return 1;
         }
-        len++;
-        if(c == ' ' || c == '\t')
-        {
-            write(1, "*", 1);
-            len = 0;
-        }
-        else
-            write(1, &c, 1);
+        print_mask(word, len1, len2, &cmp);
+        free(word);
     }
-    free(word);
-    return 0;
+	return 0;
 }
+
+
 int main(int ac, char **av)
 {
-    if(ac != 2 || ft_strlen(av[1]) == 0)
+    if(ac != 2 || strlen(av[1]) == 0)
         return 1;
-    if(space_handle(av[1]) == -1)
-    {
-        return stream_mask(av[1]);
-    }
-    else
-        return 0;
+    return stream_mask(av[1]);
 }
